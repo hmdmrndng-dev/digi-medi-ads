724c9f07d9acb6d399f0c79a26003d8790b3f945
hmdmrndng <hmdmrndng@gmail.com>
2026-04-03 14:56:22 +0800

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,10 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
 
-const connectionString = `${process.env.POSTGRES_URL_NON_POOLING}`;
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
 
-const adapter = new PrismaPg({ 
-  connectionString,
-  ssl: { rejectUnauthorized: false } 
-});
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
